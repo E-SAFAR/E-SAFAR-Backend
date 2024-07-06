b0667dcd83376103c75717cbaa1dce1dce2d04da
E-SAFAR <esafar333@gmail.com>
2024-07-06 17:06:56 +0530

    Updated

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
+
+// CORS configuration
 const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Use environment variable or fallback to localhost
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
 // import routes
 const user = require("./Controller/User");
 
 app.use(ErrorHandler);
 const connectDatabase = require("./db/Database");
-// const cloudinary = require("cloudinary");
-require("dotenv").config();
+
 // Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
@@ -34,11 +38,6 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 // connect db
 connectDatabase();
-// cloudinary.config({
-//   cloud_name: 'dw2akyum5',
-//   api_key: '813325225287557',
-//   api_secret: 'Ga8WkWni5X8t1EO-LOunfEHN-1g'
-// });
 
 // create server
 const server = app.listen(process.env.PORT, () => {
