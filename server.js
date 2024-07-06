b5aacfcbd2d0a4a57f6fe0e7069f06bfb486ffb6
E-SAFAR <esafar333@gmail.com>
2024-07-06 17:21:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,27 +4,21 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
-
-// CORS configuration
 const corsOptions = {
-  origin: "https://e-safar-frontend.vercel.app", // Use environment variable or fallback to localhost
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-
 // import routes
 const user = require("./Controller/User");
 
-app.use("/api/v2/user", user); // Ensure routes are defined after CORS middleware
-
 app.use(ErrorHandler);
 const connectDatabase = require("./db/Database");
-
+// const cloudinary = require("cloudinary");
+require("dotenv").config();
 // Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
@@ -40,11 +34,18 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 // connect db
 connectDatabase();
+// cloudinary.config({
+//   cloud_name: 'dw2akyum5',
+//   api_key: '813325225287557',
+//   api_secret: 'Ga8WkWni5X8t1EO-LOunfEHN-1g'
+// });
 
 // create server
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+
+app.use("/api/v2/user", user);
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err) => {
